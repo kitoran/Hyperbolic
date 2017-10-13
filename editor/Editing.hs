09b6b1aee26ab3ec082e9bacd1478934b33c26dd
Nikita Fufaev <none@none>
2017-10-13 14:31:36 +0300

    moe thought process in Editing.hs

--- editor/Editing.hs
+++ editor/Editing.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 module Editing where
 
 data Action
@@ -6,4 +7,24 @@ data Model
 
 data P a where
     getModel :: P Model
-    tramsform :: Action -> P ()
+    transform :: Action -> P ()
+
+
+-- в Editor.hs типа функция run :: PromptT P IO
+runrun :: IO ()
+runrun = do 
+    model <- loadOrCreateModel
+    MonadPrompt.runPrompt (\case
+        Pure -> model
+        smthElse -> blah) run
+
+run :: PromptT P IO
+run = do 
+    let ?someFrpProbably = False
+    action <- somehowWeGetWhatActionUserWantsToDo
+    prompt (transform action)
+    lift showNewModelToUser =<< prompt getModel
+    run
+
+data ViewOptions
+data EditingMode
