71a7f44969fc0a645cfd462c38fa31b528689551
Nikita Fufaev <none@none>
2017-11-05 02:11:27 +0300

    made editor use gadts, which is useless

--- editor/Editing.hs
+++ editor/Editing.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# OPTIONS_GHC -Wincomplete-patterns #-}
 -- # LANGUAGE PatternSynonyms  #
 module Editing where
@@ -13,62 +20,129 @@ import System.Exit
 -- import Data.Sequence
 import Data.IORef
 import Data.Char
+import Data.Proxy
+import Data.Singletons.TH
+import GHC.Exts
 import Control.Concurrent
 import Control.Concurrent.Chan
 import Control.Concurrent.MVar 
+import Debug.Trace
 import Control.Monad
-import qualified Linear as L
+-- import qualified Linear as L
+data Coordinates = C Int
 
-data Menu = ...
-data EditorState = AddingVertices | AddingVertices | MenuOpened Menu
-data Doing EditorState EditorState where 
+$(singletons [d|
+  data Menu = R | L
+  |])
+$(singletons [d|
+  data EditorState = AddingVertices | AddingEdges | MenuOpened Menu
+  |])
+
+data Doing (a::EditorState) (b::EditorState) where -- разбить на изменение view, изменение model и действия типа выхода ( они могут быть в одном типе с изменениями view)  
     AddVertex :: Coordinates -> Doing AddingVertices AddingVertices
-    ClickMenu :: (m::Menu) -> Doing a (MenuOpened m) -- dependent much? singletons bla bla type inference
-do :: Doing a b -> State a -> State b
+    RemoveVertex :: Coordinates -> Doing AddingEdges AddingEdges
+    ToggleModeV :: Doing AddingEdges AddingVertices
+    ToggleModeE :: Doing AddingVertices AddingEdges
+    ClickMenu :: forall a (m::Menu) . Doing a (MenuOpened m) -- dependent much? singletons bla bla type inference
+    Quit :: Doing a Any
+toggle AddingVertices = AddingEdges
+toggle  AddingEdges =AddingVertices
+toggle _ = error "this should be a type error"
+-- class Brack (e::EditorState) where
+    -- brack :: Proxy e -> Model -> String
+data Singl (e::EditorState) where
+   Av :: Singl AddingVertices
+   Ae :: Singl AddingEdges
+   Mo :: forall (m::Menu) . SMenu m -> Singl (MenuOpened (m))
+-- reify :: Singl e -> EditorState
+-- reify Av = AddingVertices
+-- reify Ae = AddingEdges
+-- reify (Mo m) = MenuOpened $ reifym m
+
+-- instance Brack AddingVertices where
+    -- brack _ m =  ("\"" ++ show m ++ "\"")
+-- instance Brack AddingEdges where
+    -- brack _ m =  ("[" ++ show m ++ "]")
+-- instance Brack Any where
+    -- brack _ m = error "this is precisely the code i don't want to write"-- ("\"" ++ show m ++ "\"")
+data State where 
+    S :: forall e. Singl e -> Model -> State  
+-- data SomeState where
+    -- SS :: forall e. State -> SomeState
+app :: Doing a b -> Doing b c -> Doing a c
+app = app
+
 type Number = Double 
-
-data Action = A 
-
-apply :: Action -> Model -> Model
-apply a = case a of A -> id
+data WRER (a::EditorState)
+-- data Action = A 
+-- type family Apply a b where
+    -- Apply (Doing a b) (WRER a) = WRER b
+-- apply :: forall a b. Doing a b -> State -> State 
+-- apply (AddVertex (C e)) (S _ (M d)) = S (Proxy @b) $ M (e+d)
+apply2 :: forall a b. Doing a b -> WRER a -> WRER b
+apply2 = apply2
+-- apply3 :: forall a b. a -> b -> (Apply a b)
+-- apply3 = apply3
 -- data Action
 
-data Model = M
-data ViewOptions = VO (L.M44 Double)deriving (Enum)
+newtype Model = M {unM::Int} deriving ( Show, Num)
 
--- data Event = Keyboard Char | Show
+-- data ViewOptions = VO (L.M44 Double) deriving (Show, Read)
+
+data Event = Keyboard Char | Show deriving ( Eq)
 
 data EditingMode = EM deriving (Enum, Eq)
 
-data State = S ViewOptions EditingMode Model 
-
+-- data State = S ViewOptions EditingMode Model 
+function :: Doing a b -> Proxy a -> Proxy b
+function = function
+type family Fam d f where
+    Fam (Doing a b) a = b
 -- data SpecialEvent = Quit | ToggleView | ToggleMode
-
+-- data Exists = forall e  t . E (Doing e t) 
+-- interpret :: forall (r::EditorState) t . Char -> Maybe (Doing r t) 
+-- interpret c = Just (AddVertex (C $ read [c]))
+pu :: forall k. Singl k
+pu = pu
+processKeyboard :: IORef (State ) -> Char -> IO Bool
 processKeyboard stateRef c = do
-    cont <- atomicModifyIORef stateRef id{-(\(S v e m) -> case interpret e c of
-        (Just (Right a)) -> (S v e $ apply a m, True)
-        (Just (Left Quit)) -> (S v e m, False)
-        (Just (Left ToggleView)) -> (S (toggle v) e m, True)
-        (Just (Left ToggleMode)) -> (S v (toggle e) m, True)
-        (Nothing) -> (S v e m, True))-}
+     atomicModifyIORef stateRef (\s@(S v m ) -> case interpret v c of
+        (Just (SD _ (AddVertex (C ff)))) -> (S Av (M $ unM m+ff), True)
+        (Just (SD _ (RemoveVertex (C ff)))) -> (S Ae (M $ unM m-ff), True)
+        (Just (SD _ Quit)) -> (S pu m, False)
+        -- (Just (Left ToggleView)) -> (S (toggle v) e m, True)
+        (Just (SD _ ToggleModeE)) -> (S (Ae) m , True)
+        (Just (SD _ ToggleModeV)) -> (S (Av) m , True)
+        (Nothing) -> (S v m , True))
 
-processEvent stateRef (Show) = do
-    (S v _ m) <- readIORef stateRef 
-    case v of
-        Quotes -> putStrLn $ "\"" ++ show m ++ "\""
-        Parens -> putStrLn $ "(" ++ show m ++ ")"
-    return True
+brack :: Singl e -> Model -> String
+brack Av m =  ("\"" ++ show m ++ "\"")
+brack Ae m =  ("[" ++ show m ++ "]")
+brack _ _ =  [] -- ("[" ++ show m ++ "]")
+processEvent stateRef event = do
+    (S v m ) <- readIORef stateRef
+    case event of
+     Show -> putStrLn (brack v m) >> return True
+            -- AddingEdges -> putStrLn ("(" ++ show m ++ ")") >> return True
+     Keyboard c -> processKeyboard stateRef c
 
-interpret e c = 
-  | isDigit c && e == AddMode = Just $ Right $ Add (read [c]) 
-  | isDigit c && e == SubtractMode = Just $ Right $ Subtract (read [c]) 
-  | (c == 'd') = Just $ Right $ Double
-  | (c == 'q') = Just $ Left Quit
-  | (c == 's') = Just $ Left ToggleMode
-  | (c == 'v') = Just $ Left ToggleView
+data SomeDoing r = forall t . SD (Singl t) (Doing r t)
+interpret :: Singl e -> Char -> Maybe (SomeDoing e)
+interpret Av c 
+  | isDigit c  = Just $ SD Av $ AddVertex (C $ read [c]) 
+  | (c == 'q') = Just $ SD undefined $ Quit
+  | (c == 's') = Just $ SD Ae $ ToggleModeE
+  | otherwise = Nothing
+interpret Ae c 
+  | isDigit c = Just $ SD Ae $ RemoveVertex (C $ read [c]) 
+  | (c == 'q') = Just $ SD undefined $ Quit
+  | (c == 's') = Just $ SD Av $ ToggleModeV -- последние две строки уместить в одну?
   | otherwise = Nothing
 
-eventLoop :: IORef State -> Chan Event -> IO ()
+  -- | (c == 'd') = Just $ Right $ Double
+  -- | (c == 'v') = Just $ Left ToggleView
+
+eventLoop :: IORef (State ) -> Chan Event -> IO ()
 eventLoop stateRef queueRef = do
     event <- readChan queueRef
     cont <- processEvent stateRef event
@@ -79,7 +153,7 @@ main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
     i <- myThreadId
-    y <- newIORef (S Quotes AddMode 5)
+    y <- newIORef (S (Av) 4)
     var <- newChan
     sho <- forkIO $ showM var
     rea <- forkIO $ readM var
@@ -87,11 +161,11 @@ main = do
     killThread sho
     killThread rea
   where
-    go :: MVar Action -> IORef Model -> IO ()
-    go y i = do 
-      d <- takeMVar y
-      modifyIORef i (apply d)
-      go y i
+    -- go :: MVar Action -> IORef Model -> IO ()
+    -- go y i = do 
+    --   d <- takeMVar y
+    --   modifyIORef i (apply d)
+    --   go y i
 
 
 readM :: Chan Event -> IO ()
